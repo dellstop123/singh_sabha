ba8710339fc06d69fd96f457740719b673ca2b9d
Guneet Singh <guneetkhalsa71@gmail.com>
2022-02-01 00:25:42 +0530

    contact form updated with database

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .form import ContactForm
+from django.contrib import messages
 # Create your views here.
 
 def index(request):
@@ -12,4 +13,12 @@ def location(request):
     return render(request,'partial/location.html') 
 
 def contact(request):
-    return render(request,'partial/contact.html')       
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Message Sent Successfully')
+            # return render(request, 'partial/contact.html')
+    form = ContactForm()
+    context = {'form': form}
+    return render(request,'partial/contact.html',context)
--- singh_sabha/settings.py
+++ singh_sabha/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
-
+from django.contrib.messages import constants as messages
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -53,6 +53,14 @@ MIDDLEWARE = [
     'django.contrib.admindocs.middleware.XViewMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 ROOT_URLCONF = 'singh_sabha.urls'
 
